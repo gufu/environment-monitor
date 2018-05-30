1e2e29155e47b30cc6d7591128031a592d56f43e
Konrad Gusz <k.gusz@cinema-city.pl>
2018-05-30 20:00:03 +0200

    added tests for various elements of the website
    response model, basic error handling, parameters

--- client/src/components/AddPost.vue
+++ client/src/components/AddPost.vue
@@ -64,4 +64,3 @@ export default {
   cursor: pointer;
 }
 </style>
-
--- server/app.js
+++ server/app.js
@@ -2,54 +2,124 @@ var express = require('express')
 var app = express()
 var request = require('request')
 var moment = require('moment')
+var config = require('./sites-config.json')
 
 const http = require('http')
 const https = require('https')
-// const htmlparser = require('htmlparser2')
 const cheerio = require('cheerio')
-// const logger = require('logger').createLogger()
+const axios = require('axios')
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
-
-// <!-- VERSION: ver-2.5.14.2 -->
 app.get('/fetch', function (req, res, next) {
-  let url = Object.keys(req.query)[0]
-  if (typeof url === 'undefined') {
-    res.send({message: /url cannot be undefined/})
+  let queriedProject = Object.keys(req.query)[0]
+
+  let tenant = config.find(x => x.project === queriedProject)
+  if (typeof tenant !== 'undefined') {
+    let url = tenant.websiteUrl
+    if (typeof url === 'undefined') {
+      res.json({error: 'URL_UNDEFINED', message: 'url cannot be undefined'})
+      res.end()
+    } else {
+      req.query.url = url.replace(/.*?:\/\//g, '')
+      let options = tenant
+      return processResponse(req, res, options)
+    }
+  } else {
+    res.json({error: 'PROJECT_UNDEFINED', message: 'no project found for specified id'})
+    res.end()
+    // res.end('Cannot ' + req.method + ' ' + req.url)
   }
-  req.query.url = url.replace(/.*?:\/\//g, '')
-  let options = {
-    hostname: url
-  }
-  processResponse(req, res, options)
 })
 
-let processResponse = function (req, res, options) {
+let processResponse = async function (req, res, options) {
   let time = moment().format('YYYY-MM-DD HH:mm:ss ZZ')
-  var responseObject = {
-    website: options.hostname,
-    port: options.port,
+  let futureDate = moment().add(1, 'y').format('YYYY-MM-DD')
+  let responseObject = {
+    project: options.project,
+    website: options.websiteUrl,
     version: '',
     lastChecked: time
   }
-  request(options.hostname, function (error, response, html) {
-    var $ = cheerio.load(html, {xmlMode: true})
+
+  // get dynamic data for quickbook and posters
+  let bookNowPosters = await getPosters(options.websiteUrl + options.bookNowPostersUrl)
+  let comingSoonPosters = await getPosters(options.websiteUrl + options.comingSoonPostersUrl)
+  let quickbookCinemas = await getCinemasForQuickbook(options.websiteUrl + options.quickbookCinemasUrl + futureDate)
+  responseObject['bookNowPosters'] = bookNowPosters
+  responseObject['comingSoonPosters'] = comingSoonPosters
+  responseObject['quickbookCinemas'] = quickbookCinemas
+
+  request(options.websiteUrl, function (error, response, html) {
+    let $ = cheerio.load(html, {xmlMode: true})
     if (!error && response.statusCode === 200) {
-      $('body').contents().filter(function () {
-        return this.nodeType === 8
-      }).each(function (i, e) {
-        let comment = e.nodeValue
-        if (comment.toLowerCase().indexOf('version') > 0) {
-          responseObject['version'] = comment.split('ver-')[1].trim()
-        }
-      })
+      responseObject['version'] = getAppVersion($)
+      responseObject['menuElements'] = getMenuElements($)
+      responseObject['heroBanners'] = getHeroBanners($)
+      responseObject['promoboxFirstTabItems'] = getPromoboxCards($)
+      responseObject['footerLinks'] = footerLinks($)
       res.send(responseObject)
     } else {
       res.send(responseObject)
     }
   })
+}
+
+let getAppVersion = function ($) {
+  let response = ''
+  $('body').contents().filter(function () {
+    return this.nodeType === 8
+  }).each(function (i, e) {
+    let comment = e.nodeValue
+    if (comment.toLowerCase().indexOf('version') > 0) {
+      response = comment.split('ver-')[1].trim()
+    }
+  })
+  return response
+}
+
+let getMenuElements = function ($) {
+  let $expIcons = $('#defaultmenu .nav.navbar-right .exp-icon-container a') // for uk
+  let $menuItems = $('#defaultmenu ul.nav li:not(.session-cinema)')
+  return $expIcons.length + $menuItems.length
+}
+
+let getHeroBanners = function ($) {
+  return $('#slick-hero a').length
+}
+
+let getPosters = async function (url) {
+  let result = ''
+  let response = await axios.get(url)
+  let data = response.data
+  let body = data.body
+  if (typeof body !== 'undefined' && typeof body['posters'] !== 'undefined') {
+    result = body.posters.length
+  } else {
+    result = 'error'
+  }
+  return result
+}
+
+let getCinemasForQuickbook = async function (url) {
+  let result = ''
+  let response = await axios.get(url)
+  let data = response.data
+  let body = data.body
+  if (typeof body !== 'undefined' && typeof body['cinemas'] !== 'undefined') {
+    result = body['cinemas'].length
+  } else {
+    result = 'error'
+  }
+  return result
+}
+
+let getPromoboxCards = function ($) {
+  let cards = $('#tab_promoTab1 .hidden-xs .promobox-card').length
+  let links = $('#tab_promoTab1 .hidden-xs .promobox-link').length
+  return cards + links
+}
+
+let footerLinks = function ($) {
+  return $('section.footer .hidden-xs .footer-group-item').length
 }
 
 app.set('port', process.env.PORT || 8081)
